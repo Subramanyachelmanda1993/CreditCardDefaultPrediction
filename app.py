3c22f101c42fa91b0fad2cde9295a6c738344976
SubramanyaChelmanda <subramanyazidane@gmail.com>
2022-11-30 17:06:10 +0530

    changes made

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app=Flask(__name__)
 
 @app.route("/", methods=['GET', 'POST'])
 def index():
-    return "Starting intership project"
+    return "Starting Ineuron's Intership Project"
   
 if __name__ == '__main__':
     app.run(debug=True)
