e51d1c977046f37c8c243abffba8c87e1588ee12
SubramanyaChelmanda <subramanyazidane@gmail.com>
2022-12-02 10:56:49 +0530

    tested

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
 from flask import Flask
+from creditcarddefault.logger import logging
+from creditcarddefault.exception import CreditCradDefaultException
+import sys
 
 app=Flask(__name__)
 
 @app.route("/", methods=['GET', 'POST'])
 def index():
-    return "Starting Ineuron's Intership Project"
+    try:
+        raise Exception("testing custom exception")
+    except Exception as e:
+        default=CreditCardDefault(e,sys)
+        logging.info(creditcarddefault.error_message)
+        logging.info("testing logging module")
+        return "Starting Ineuron's Intership Project"
   
 if __name__ == '__main__':
     app.run(debug=True)
--- creditcarddefault/exception/__init__.py
+++ creditcarddefault/exception/__init__.py
@@ -1,7 +1,7 @@
 import os
 import sys
 
-class HousingException(Exception):
+class CreditCradDefaultException(Exception):
 
     def __init__(self, error_message:Exception, error_detail:sys):
         super().__init__(error_message)
