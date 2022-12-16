ced92c618088123571cbd65957d7bd11bf03a512
SubramanyaChelmanda <subramanyazidane@gmail.com>
2022-12-16 10:42:21 +0530

    source file added

--- creditcarddefault/component/data_ingestion.py
+++ creditcarddefault/component/data_ingestion.py
@@ -24,26 +24,26 @@ class DataIngestion:
             download_url = self.data_ingestion_config.dataset_download_url
 
             #folder location to download file
-            tgz_download_dir = self.data_ingestion_config.tgz_download_dir
+            csv_download_dir = self.data_ingestion_config.csv_download_dir
 
-            if os.path.exists(tgz_download_dir):
-                os.remove(tgz_download_dir)
+            if os.path.exists(csv_download_dir):
+                os.remove(csv_download_dir)
             
-            os.makedirs(tgz_download_dir, exist_ok=True)
+            os.makedirs(csv_download_dir, exist_ok=True)
 
             creditcarddefault_file_name = os.path.basename(download_url)
 
-            tgz_file_path = os.path.join(tgz_download_dir, creditcarddefault_file_name)
+            csv_file_path = os.path.join(csv_download_dir, creditcarddefault_file_name)
 
-            logging.info(f"Downloading file from :[{download_url}] into :[{tgz_file_path}]")
-            urllib.request.urlretrieve(download_url, tgz_file_path)
-            logging.info(f"File :[{tgz_file_path}] has been downloaded successfully.")
-            return tgz_file_path
+            logging.info(f"Downloading file from :[{download_url}] into :[{csv_file_path}]")
+            urllib.request.urlretrieve(download_url, csv_file_path)
+            logging.info(f"File :[{csv_file_path}] has been downloaded successfully.")
+            return csv_file_path
 
         except Exception as e:
                 raise CreditCardDefaultException(e,sys) from e
 
-    def extract_tgz_file(self,tgz_file_path:str):
+    def extract_csv_file(self,csv_file_path:str):
         try:
             raw_data_dir = self.data_ingestion_config.raw_data_dir
 
@@ -52,10 +52,10 @@ class DataIngestion:
 
             os.makedirs(raw_data_dir,exist_ok=True)
 
-            logging.info(f"Extracting tgz file: [{tgz_file_path}] into dir: [{raw_data_dir}]")
-            with tarfile.open(tgz_file_path) as creditcard_tgz_file_obj:
-                creditcard_tgz_file_obj.extractall(path=raw_data_dir)
-            logging.info(f"Extraction completed")
+            # logging.info(f"Extracting tgz file: [{tgz_file_path}] into dir: [{raw_data_dir}]")
+            # with tarfile.open(tgz_file_path) as creditcard_tgz_file_obj:
+            #     creditcard_tgz_file_obj.extractall(path=raw_data_dir)
+            # logging.info(f"Extraction completed")
 
         except Exception as e:
             raise CreditCardDefaultException(e,sys) from e 
